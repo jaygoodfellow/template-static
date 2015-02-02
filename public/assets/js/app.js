b59b1f5745ff92637b223d230b37e04547ea998f
Jay Goodfellow <j.goodfellow@arcane.ws>
2015-02-02 09:54:27 -0500

    added bootstrap/jquery

---
+++ public/assets/js/app.js
@@ -0,0 +1,37 @@
+
+var require = {
+    shim : {
+        "bootstrap" : { "deps" :['jquery'] }
+    },
+    paths: {
+        "jquery" : "/js/vendor/jquery-1.11.1.min",
+        "bootstrap" :  "/js/vendor/bootstrap.min.js"  
+    }
+};
+
+
+require(['jquery', 'bootstrap'], function($){
+	// Avoid `console` errors in browsers that lack a console.
+	(function() {
+	    var method;
+	    var noop = function () {};
+	    var methods = [
+	        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+	        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+	        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+	        'timeStamp', 'trace', 'warn'
+	    ];
+	    var length = methods.length;
+	    var console = (window.console = window.console || {});
+
+	    while (length--) {
+	        method = methods[length];
+
+	        // Only stub undefined methods.
+	        if (!console[method]) {
+	            console[method] = noop;
+	        }
+	    }
+ 		//jquery code goes here
+     });
+});
