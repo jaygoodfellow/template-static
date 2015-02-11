c4dd62f7a8790f99aa302b45948af86a6b464ca0
Jay Goodfellow <j.goodfellow@arcane.ws>
2015-02-11 15:29:34 -0500

    removed gulp-git functionality for now

--- setup.bat
+++ setup.bat
@@ -1,7 +1,4 @@
-call npm install gulp del gulp-git gulp-ruby-sass@1.0.0-alpha gulp-minify-css gulp-notify gulp-uglify gulp-autoprefixer gulp-concat watchify browserify vinyl-source-stream
-call gulp clone
-call gulp move
-call gulp clean
+call npm install gulp gulp-ruby-sass@1.0.0-alpha gulp-minify-css gulp-notify gulp-uglify gulp-autoprefixer gulp-concat watchify browserify vinyl-source-stream
 call gulp fonts
 call gulp styles
 call gulp default
